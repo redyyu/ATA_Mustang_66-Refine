eabe05c5897f6fc4f4012d8792cd8b924c574b91
redyyu <redy.ru@gmail.com>
2024-03-09 16:38:58 +0800

    xxx

--- media/lua/shared/ATAMustang66_SpawnList.lua
+++ media/lua/shared/ATAMustang66_SpawnList.lua
@@ -1,5 +1,5 @@
 VehicleZoneDistribution = VehicleZoneDistribution or {};
 
-VehicleZoneDistribution.parkingstall.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.good.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.good.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 1};
+VehicleZoneDistribution.parkingstall.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 5};
+VehicleZoneDistribution.good.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 5};
+VehicleZoneDistribution.good.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 5};
