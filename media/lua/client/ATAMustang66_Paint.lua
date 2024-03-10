84ce0cd26bd1fad968344e42512d6baec60b1ec3
redyyu <redy.ru@gmail.com>
2024-03-10 16:26:42 +0800

    refact paint car

--- media/lua/client/ATAMustang66_Paint.lua
+++ media/lua/client/ATAMustang66_Paint.lua
@@ -1,40 +1,41 @@
+require "TimedActions/ISPaintVehicleAction.lua"
 
 local vehicleTable = {
-    ["Base.ATAMustang66"] = 1,
-    ["Base.ATAMustang66Custom"] = 1
+    ["Base.ATAMustang66"] = "PaintRed",
+    ["Base.ATAMustang66Custom"] = "PaintRed",
 }
 
-local paintTable = {
-    ["PaintWhite"] = 0,
-    ["PaintGrey"] = 2,
-    ["PaintBlack"] = 4,
-    ["PaintPink"] = 6,
-    ["PaintRed"] = 8,
-    ["PaintPurple"] = 10,
-    ["PaintBlue"] = 12,
-    ["PaintLightBlue"] = 14,
-    ["PaintCyan"] = 16,
-    ["PaintTurquoise"] = 18,
-    ["PaintGreen"] = 20,
-    ["PaintLightBrown"] = 22,
-    ["PaintBrown"] = 24,
-    ["PaintYellow"] = 26,
-    ["PaintOrange"] = 28,
-}
+-- local paintTable = {
+--     ["PaintWhite"] = 0,
+--     ["PaintGrey"] = 2,
+--     ["PaintBlack"] = 4,
+--     ["PaintPink"] = 6,
+--     ["PaintRed"] = 8,
+--     ["PaintPurple"] = 10,
+--     ["PaintBlue"] = 12,
+--     ["PaintLightBlue"] = 14,
+--     ["PaintCyan"] = 16,
+--     ["PaintTurquoise"] = 18,
+--     ["PaintGreen"] = 20,
+--     ["PaintLightBrown"] = 22,
+--     ["PaintBrown"] = 24,
+--     ["PaintYellow"] = 26,
+--     ["PaintOrange"] = 28,
+-- }
 
 local PaintVehicle = {}
 PaintVehicle.ghs = " <RGB:" .. getCore():getGoodHighlitedColor():getR() .. "," .. getCore():getGoodHighlitedColor():getG() .. "," .. getCore():getGoodHighlitedColor():getB() .. "> "
 PaintVehicle.bhs = " <RGB:" .. getCore():getBadHighlitedColor():getR() .. "," .. getCore():getBadHighlitedColor():getG() .. "," .. getCore():getBadHighlitedColor():getB() .. "> "
 
 
-PaintVehicle.paintBus = function(playerObj, vehicle, newSkinIndex, paintBrush, paintCan, uses)
-    if paintBrush and paintCan and paintCan:getCurrentUses() * 10 >= uses then
-        ISWorldObjectContextMenu.transferIfNeeded(playerObj, paintBrush)
-        ISWorldObjectContextMenu.transferIfNeeded(playerObj, paintCan)
-        ISTimedActionQueue.add(ISPathFindAction:pathToVehicleArea(playerObj, vehicle, "Engine"))
-        ISTimedActionQueue.add(ISPaintVehicleAction:new(playerObj, vehicle, "Engine", newSkinIndex, paintCan, uses, 50))
-    end
-end
+-- PaintVehicle.paintVehicle = function(playerObj, vehicle, newSkinIndex, paintBrush, paintCan, uses)
+--     if paintBrush and paintCan and paintCan:getCurrentUses() * 10 >= uses then
+--         ISWorldObjectContextMenu.transferIfNeeded(playerObj, paintBrush)
+--         ISWorldObjectContextMenu.transferIfNeeded(playerObj, paintCan)
+--         ISTimedActionQueue.add(ISPathFindAction:pathToVehicleArea(playerObj, vehicle, "Engine"))
+--         ISTimedActionQueue.add(ISPaintVehicleAction:new(playerObj, vehicle, "Engine", newSkinIndex, paintCan, uses, 50))
+--     end
+-- end
 
 PaintVehicle.paintWords = function(playerObj, vehicle, newSkinIndex, paintBrush, paintCan)
     if paintBrush and paintCan then
@@ -58,8 +59,9 @@ end
 
 PaintVehicle.doFillMenuOutsideVehicle = function(playerObj, context, vehicle, test)
     local playerInv = playerObj:getInventory()
-    
-    if (vehicleTable[vehicle:getScriptName()]) then 
+    local graffitiType = vehicleTable[vehicle:getScriptName()]
+
+    if (graffitiType) then 
         
         local paintMenuOpt = context:addOption(getText("ContextMenu_Vehicle_PAINT"), nil, nil)
         local subMenuAvailable = false
@@ -70,7 +72,7 @@ PaintVehicle.doFillMenuOutsideVehicle = function(playerObj, context, vehicle, te
 
         if (vehicle:getSkinIndex()%2) == 0 then
             local paintBrush = playerInv:getFirstTypeRecurse("Paintbrush")
-            local paintCan = playerInv:getFirstTypeRecurse("PaintRed")
+            local paintCan = playerInv:getFirstTypeRecurse(graffitiType)
             writeOpt = subMenu:addOptionOnTop(getText("ContextMenu_Vehicle_STRAYS"), 
                                               playerObj, PaintVehicle.paintWords, vehicle, 
                                               vehicle:getSkinIndex()+1, paintBrush, paintCan)
@@ -91,9 +93,9 @@ PaintVehicle.doFillMenuOutsideVehicle = function(playerObj, context, vehicle, te
                 
                 if paintCan then
                     have_uses = math.floor(paintCan:getCurrentUses() * 10) -- less 1 unit, otherwise will be the other item as empty.
-                    desc_write = desc_write .. PaintVehicle.ghs..getText("Tooltip_Item_PaintRed") .. " ".. have_uses .."/1 unit <LINE> "
+                    desc_write = desc_write .. PaintVehicle.ghs..getText("Tooltip_Item_"..graffitiType) .. " ".. have_uses .."/1 unit <LINE> "
                 else
-                    desc_write = desc_write .. PaintVehicle.bhs..getText("Tooltip_Item_PaintRed") .. " ".. have_uses .."/1 unit <LINE> "
+                    desc_write = desc_write .. PaintVehicle.bhs..getText("Tooltip_Item_"..graffitiType) .. " ".. have_uses .."/1 unit <LINE> "
                     writeOpt.onSelect = nil
                     writeOpt.notAvailable = true
                 end
@@ -129,48 +131,53 @@ PaintVehicle.doFillMenuOutsideVehicle = function(playerObj, context, vehicle, te
             end
         end
 
-        -- Paint car
+        -- DO NOT paint full car, in the end of day, it is not reasonable.
+        -- -- Paint car
 
-        for k, v in pairs(paintTable) do
-            local paintBrush = playerInv:getFirstTypeRecurse("Paintbrush")
-            local paintCan = playerInv:getFirstTypeRecurse(k)
-            local uses = 10
+        -- if playerObj:getPerkLevel(Perks.Woodwork) < 10 and not isDebugEnabled() then
+        --     return
+        -- end
+
+        -- for k, v in pairs(paintTable) do
+        --     local paintBrush = playerInv:getFirstTypeRecurse("Paintbrush")
+        --     local paintCan = playerInv:getFirstTypeRecurse(k)
+        --     local uses = 10
             
-            local opt = subMenu:addOption(getText(k), playerObj, PaintVehicle.paintBus, vehicle, v, paintBrush, paintCan, uses)
-            opt.toolTip = ISWorldObjectContextMenu.addToolTip()
-            opt.toolTip:setName(getText(k))
+        --     local opt = subMenu:addOption(getText(k), playerObj, PaintVehicle.paintVehicle, vehicle, v, paintBrush, paintCan, uses)
+        --     opt.toolTip = ISWorldObjectContextMenu.addToolTip()
+        --     opt.toolTip:setName(getText(k))
             
-            local tooltip_desc = getText("Tooltip_Vehicle_PAINT") .. "<LINE><LINE>"
+        --     local tooltip_desc = getText("Tooltip_Vehicle_PAINT") .. "<LINE><LINE>"
 
-            if paintBrush then
-                tooltip_desc = tooltip_desc .. PaintVehicle.ghs .. getText("Tooltip_Item_Paintbrush") .. " <LINE>"
-            else
-                tooltip_desc = tooltip_desc .. PaintVehicle.bhs .. getText("Tooltip_Item_Paintbrush") .. " <LINE>"
-                opt.onSelect = nil
-                opt.notAvailable = true
-            end
+        --     if paintBrush then
+        --         tooltip_desc = tooltip_desc .. PaintVehicle.ghs .. getText("Tooltip_Item_Paintbrush") .. " <LINE>"
+        --     else
+        --         tooltip_desc = tooltip_desc .. PaintVehicle.bhs .. getText("Tooltip_Item_Paintbrush") .. " <LINE>"
+        --         opt.onSelect = nil
+        --         opt.notAvailable = true
+        --     end
 
-            local have_uses = 0
-            if paintCan then
-                have_uses = math.floor(paintCan:getCurrentUses() * 10)
-            end
+        --     local have_uses = 0
+        --     if paintCan then
+        --         have_uses = math.floor(paintCan:getCurrentUses() * 10)
+        --     end
             
-            if paintCan and have_uses >= uses then
-                tooltip_desc = tooltip_desc .. PaintVehicle.ghs .. getText("Tooltip_Item_"..k)
-                tooltip_desc = tooltip_desc .. " " .. have_uses .."/" .. uses .. " unit <LINE>"
-            else
-                tooltip_desc = tooltip_desc .. PaintVehicle.bhs .. getText("Tooltip_Item_"..k)
-                tooltip_desc = tooltip_desc .. " " .. have_uses .."/" .. uses .. " unit <LINE>"
-                opt.onSelect = nil
-                opt.notAvailable = true
-            end
+        --     if paintCan and have_uses >= uses then
+        --         tooltip_desc = tooltip_desc .. PaintVehicle.ghs .. getText("Tooltip_Item_"..k)
+        --         tooltip_desc = tooltip_desc .. " " .. have_uses .."/" .. uses .. " unit <LINE>"
+        --     else
+        --         tooltip_desc = tooltip_desc .. PaintVehicle.bhs .. getText("Tooltip_Item_"..k)
+        --         tooltip_desc = tooltip_desc .. " " .. have_uses .."/" .. uses .. " unit <LINE>"
+        --         opt.onSelect = nil
+        --         opt.notAvailable = true
+        --     end
 
-            opt.toolTip.description = tooltip_desc
+        --     opt.toolTip.description = tooltip_desc
 
-            if not opt.notAvailable then
-                subMenuAvailable = true
-            end
-        end
+        --     if not opt.notAvailable then
+        --         subMenuAvailable = true
+        --     end
+        -- end
 
         if not subMenuAvailable then
             paintMenuOpt.notAvailable = true
--- media/lua/shared/ATAMustang66_SpawnList.lua
+++ media/lua/shared/ATAMustang66_SpawnList.lua
@@ -1,7 +1,50 @@
+--[[
+Use this file to alter or add vehicle spawning logic.
+ the type should be the one you define in the zone in WorldEd (VehicleZoneDistribution.trailerpark will be used for trailerpark zone type)
+ if no type is defined in the zone, parkingstall is used instead.
+ 
+ When adding a car, you define it's skin index defined in the vehicle's template (-1 mean a random skin in the skin list)
+ spawnChance is used to define the odds of spawning this car or another (the total for a zone should always be 100)
+ 
+ You have a range of variable to configure your spawning logic:
+ * chanceToPartDamage : Chance of having a damaged part, this number is added to the inventory item's damaged spawn chance of the part (so an old tire will have more chance to be damaged than a good one in a same zone). Default is 0.
+ * baseVehicleQuality : Define the base quality for part, if a part should be spawned as damaged, this will define it's max condition (so a 0.7 mean if a part spawn as damaged, it's max condition will be 70%). Default is 1.0.
+ * chanceToSpawnSpecial : Use this to define a random chance of spawning special car (picked randomly in every type with specialCar = true) on a zone. Default is 5.
+ * chanceToSpawnBurnt : Use this to define a random chance of spawning burnt car like in junkyard (picked randomly at 80% in normalburnt list & 20% in specialburnt list) on a zone. Default is 0.
+ * chanceToSpawnNormal : Use this to define a random chance of spawning a normal car (will be picked in the parkingstall zone). Used so the special parking lots don't have only special cars (so a spiffo parking lot will have lots of normal car, and sometimes a spiffo van). Default is 80(%).
+ * spawnRate : Base chance of adding a vehicle in a zone, default is 16(%).
+ * chanceOfOverCar : Chance to spawn another car over the spawned one (used in trailerpark). Default is 0.
+ * randomAngle : Are cars aligned on a grid or random angle. Default is false.
+ * chanceToSpawnKey : Define the chance to spawn a key for this car (either on the ground, directly in the car, in a near zombie or container...) Default is 70(%).
+ * specialCar : Define if the car is a special one (police, fire dept...) used to get a list of special car when trying to spawn a special car if chanceToSpawnSpecial is triggered. a special car will also make the corresponding vehicle's key not colored. Can still be used as a normal zone.
+ ]]
+--
+
 VehicleZoneDistribution = VehicleZoneDistribution or {};
 
-VehicleZoneDistribution.parkingstall.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.good.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.good.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 1};
-VehicleZoneDistribution.sport.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 5};
-VehicleZoneDistribution.sport.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 5};
+-- VehicleZoneDistribution.trailerpark.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 0.5}
+-- VehicleZoneDistribution.trailerpark.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 0.5}
+
+VehicleZoneDistribution.parkingstall.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 1}
+VehicleZoneDistribution.parkingstall.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 1}
+
+VehicleZoneDistribution.good.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 2}
+VehicleZoneDistribution.good.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 2}
+
+VehicleZoneDistribution.sport.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 5}
+VehicleZoneDistribution.sport.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 5}
+
+VehicleZoneDistribution.junkyard.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 1}
+VehicleZoneDistribution.junkyard.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 1}
+
+VehicleZoneDistribution.trafficjamw.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 0.5}
+VehicleZoneDistribution.trafficjamw.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 0.5}
+
+VehicleZoneDistribution.trafficjame.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 0.5}
+VehicleZoneDistribution.trafficjame.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 0.5}
+
+VehicleZoneDistribution.trafficjamn.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 0.5}
+VehicleZoneDistribution.trafficjamn.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 0.5}
+
+VehicleZoneDistribution.trafficjams.vehicles["Base.ATAMustang66"] = {index = -1, spawnChance = 0.5}
+VehicleZoneDistribution.trafficjams.vehicles["Base.ATAMustang66Custom"] = {index = -1, spawnChance = 0.5}
